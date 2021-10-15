a102271715b2b772e40d8433469cddc22fd5b5f1
Peter Fluit <fluitpd@gmail.com>
2021-10-15 12:09:49 -0600

    Finishing Beginner Scripting

---
+++ Unity Project/Assets/Scripts/12.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+int sum1 = 100 + 50;        // 150 (100 + 50)
+int sum2 = sum1 + 250;      // 400 (150 + 250)
+int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+public class 12 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
