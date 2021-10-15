ae702cea91efd4197133d34d692057435df30afd
Peter Fluit <fluitpd@gmail.com>
2021-10-15 11:41:22 -0600

    Script2

---
+++ Unity Project/Assets/Scripts/2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+type variableName = value;
+
+public class 2 : MonoBehaviour
+{
+    string name = "John";
+    Console.WriteLine(name);
+}
